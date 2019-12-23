660066c2124a98d2550cf57a26e76c081421240d
sa6le <dear.sobolev@gmail.com>
2019-12-23 19:14:24 +0300

    Изменил файл

--- projects/led_test/main.c
+++ projects/led_test/main.c
@@ -14,20 +14,21 @@ int main(void)
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
   /* Init LEDs */
-  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14;
+  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_12 | GPIO_Pin_15 | GPIO_Pin_14;
   GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_OUT;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_25MHz;
   GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
   GPIO_Init(GPIOD, &GPIO_InitStructure);
 
   /* Turn all the leds off */
-  GPIO_SetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14);
+
 
   while (1)
   {
     int i;
-
+    int c;
+    int k;
     /* Switch the LED on */
     GPIO_ResetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14);
     for (i = 0; i < SWITCH_DELAY; i++);
